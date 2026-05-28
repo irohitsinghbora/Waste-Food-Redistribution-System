f682f5ef73feecf764017f3e0aedd8081854f533
Rohit Singh Bora <irohitsinghbora@gmail.com>
2026-05-28 20:41:47 +0530

    Delete src directory

--- src/main/java/com/wastefood/redistribution/repository/FoodDonationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wastefood.redistribution.repository;
-
-import com.wastefood.redistribution.entity.FoodDonation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodDonationRepository extends JpaRepository<FoodDonation, Long> {
-}
